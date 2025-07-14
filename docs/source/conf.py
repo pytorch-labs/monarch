7d7535aceb0c76524c9b33c15e2929f7140a92a8
sekyonda <127536312+sekyondaMeta@users.noreply.github.com>
2025-07-14 15:35:44 -0400

    Docs Build 1
    Doc build 1

---
+++ docs/source/conf.py
@@ -0,0 +1,103 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Monarch'
+copyright = '2025, ""'
+author = '""'
+release = '""'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx_design",
+    "sphinx_sitemap",
+    "sphinxcontrib.mermaid",
+    "pytorch_sphinx_theme2",
+    "sphinxext.opengraph",]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+import pytorch_sphinx_theme2
+
+html_theme = "pytorch_sphinx_theme2"
+html_theme_path = [pytorch_sphinx_theme2.get_html_theme_path()]
+
+ogp_site_url = "http://pytorch.org/monarch"
+ogp_image = "https://pytorch.org/assets/images/social-share.jpg"
+
+html_theme_options = {
+    "navigation_with_keys": False,
+    "analytics_id": "GTM-T8XT4PS",
+    "logo": {
+        "text": "",
+    },
+    "icon_links": [
+        {
+            "name": "X",
+            "url": "https://x.com/PyTorch",
+            "icon": "fa-brands fa-x-twitter",
+        },
+        {
+            "name": "GitHub",
+            "url": "https://github.com/sekyondaMeta/monarch",
+            "icon": "fa-brands fa-github",
+        },
+        {
+            "name": "Discourse",
+            "url": "https://dev-discuss.pytorch.org/",
+            "icon": "fa-brands fa-discourse",
+        },
+        {
+            "name": "PyPi",
+            "url": "https://pypi.org/project/monarch/",
+            "icon": "fa-brands fa-python",
+        },
+    ],
+    "use_edit_page_button": True,
+    "navbar_center": "navbar-nav",
+}
+
+theme_variables = pytorch_sphinx_theme2.get_theme_variables()
+templates_path = [
+    "_templates",
+    os.path.join(os.path.dirname(pytorch_sphinx_theme2.__file__), "templates"),
+]
+
+html_context = {
+    "theme_variables": theme_variables,
+    "display_github": True,
+    "github_url": "https://github.com",
+    "github_user": "sekyondaMeta",
+    "github_repo": "monarch",
+    "feedback_url": "https://github.com/sekyondaMeta/monarch",
+    "github_version": "main",
+    "doc_path": "docs/source",
+    "library_links": theme_variables.get("library_links", []),
+    "community_links": theme_variables.get("community_links", []),
+    "language_bindings_links": html_theme_options.get("language_bindings_links", []),
+}
+
+#not sure if this is needed
+myst_enable_extensions = [
+    "colon_fence",
+    "deflist",
+    "html_image",
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,48 @@
+.. Monarch documentation master file, created by
+   sphinx-quickstart on Tue Jul  8 16:51:00 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Monarch's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+   
+   get_started
+   
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+   
+   tutorials
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+   
+   api
+   
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+
+   howtos
+
+.. toctree::
+   :maxdepth: 1
+   :hidden:
+
+   glossary
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
