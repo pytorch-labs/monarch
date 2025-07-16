c80766c4b23baf0b3ee7af1e865ce3c1e8af61ca
Peng Zhang <pzhang@meta.com>
2025-07-16 13:03:33 -0700

    Lift the `send-to-actor` logic from  `_ActorMeshRefImpl` to `ActorIdRef` (#555)
    Summary:
    
    `RDMAManger` and `DebugManager` have use cases that need to send messages directly to an actor based on its actor id, instead of to a mesh. Currently the "send-to-actor" function is piggybacking `_ActorMeshRefImpl`'s implementation. This is blocking the migration from `_ActorMeshRefImpl` to `PythonActorMesh`, because we cannot create a mesh solely based on its actor ID. 
    
    To unblock, this diff lifts the `send-to-actor` logic from  `_ActorMeshRefImpl` to `ActorIdRef`, so it is decoupled from `_ActorMeshRefImpl` and `ActorMeshRef`.
    
    Differential Revision: D78302352

--- python/monarch/rdma.py
+++ python/monarch/rdma.py
@@ -14,13 +14,7 @@ from typing import cast, Dict, Optional, Tuple
 import torch
 
 from monarch._rust_bindings.monarch_hyperactor.proc import ActorId
-from monarch._src.actor.actor_mesh import (
-    _ActorMeshRefImpl,
-    Actor,
-    ActorMeshRef,
-    endpoint,
-    MonarchContext,
-)
+from monarch._src.actor.actor_mesh import Actor, ActorIdRef, endpoint, MonarchContext
 
 
 @dataclass
@@ -56,12 +50,9 @@ class RDMAManager(Actor):
         ctx = MonarchContext.get()
         return cast(
             RDMAManager,
-            ActorMeshRef(
+            ActorIdRef(
                 RDMAManager,
-                _ActorMeshRefImpl.from_actor_id(
-                    ctx.mailbox,
-                    ActorId.from_string(f"{proc_id}.rdma_manager[0]"),
-                ),
+                ActorId.from_string(f"{proc_id}.rdma_manager[0]"),
                 ctx.mailbox,
             ),
         )
