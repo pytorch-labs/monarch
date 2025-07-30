1f090989deb8d60105a9f3a629b53e6704e33cfc
Dennis van der Staay <dstaay@meta.com>
2025-07-29 21:49:50 -0700

    RDMAXCEL - extended, accelerated - WQE. CQE and DoorBells (#541)
    Summary:
    
    This is basic cuda logic to support writing/reading basic data structures used for ibverbs comms.  Effectively this same as ibv_send/ibv_recv but written so that is can executed on cpu or cuda.
    
    Reviewed By: allenwang28
    
    Differential Revision: D78362879

--- rdmaxcel-sys/build.rs
+++ rdmaxcel-sys/build.rs
@@ -7,7 +7,65 @@
  */
 
 use std::env;
+use std::path::Path;
 use std::path::PathBuf;
+
+use glob::glob;
+use which::which;
+
+// Translated from torch/utils/cpp_extension.py
+fn find_cuda_home() -> Option<String> {
+    // Guess #1
+    let mut cuda_home = env::var("CUDA_HOME")
+        .ok()
+        .or_else(|| env::var("CUDA_PATH").ok());
+
+    if cuda_home.is_none() {
+        // Guess #2
+        if let Ok(nvcc_path) = which("nvcc") {
+            // Get parent directory twice
+            if let Some(cuda_dir) = nvcc_path.parent().and_then(|p| p.parent()) {
+                cuda_home = Some(cuda_dir.to_string_lossy().into_owned());
+            }
+        } else {
+            // Guess #3
+            if cfg!(windows) {
+                // Windows code
+                let pattern = r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v*.*";
+                let cuda_homes: Vec<_> = glob(pattern).unwrap().filter_map(Result::ok).collect();
+                if !cuda_homes.is_empty() {
+                    cuda_home = Some(cuda_homes[0].to_string_lossy().into_owned());
+                } else {
+                    cuda_home = None;
+                }
+            } else {
+                // Not Windows
+                let cuda_candidate = "/usr/local/cuda";
+                if Path::new(cuda_candidate).exists() {
+                    cuda_home = Some(cuda_candidate.to_string());
+                } else {
+                    cuda_home = None;
+                }
+            }
+        }
+    }
+    cuda_home
+}
+
+fn emit_cuda_link_directives(cuda_home: &str) {
+    let stubs_path = format!("{}/lib64/stubs", cuda_home);
+    if Path::new(&stubs_path).exists() {
+        println!("cargo:rustc-link-search=native={}", stubs_path);
+    } else {
+        let lib64_path = format!("{}/lib64", cuda_home);
+        if Path::new(&lib64_path).exists() {
+            println!("cargo:rustc-link-search=native={}", lib64_path);
+        }
+    }
+
+    println!("cargo:rustc-link-lib=cuda");
+    println!("cargo:rustc-link-lib=cudart");
+}
 
 fn main() {
     // Tell cargo to look for shared libraries in the specified directory
@@ -20,8 +78,13 @@ fn main() {
     // Link against the mlx5 library
     println!("cargo:rustc-link-lib=mlx5");
 
+    // Link against cuda library library
+    if let Some(cuda_home) = find_cuda_home() {
+        emit_cuda_link_directives(&cuda_home);
+    }
+
     // Tell cargo to invalidate the built crate whenever the wrapper changes
-    println!("cargo:rerun-if-changed=src/wrapper.h");
+    println!("cargo:rerun-if-changed=src/rdmaxcel.h");
 
     // Add cargo metadata
     println!("cargo:rustc-cfg=cargo");
@@ -30,7 +93,7 @@ fn main() {
     // The bindgen::Builder is the main entry point to bindgen
     let bindings = bindgen::Builder::default()
         // The input header we would like to generate bindings for
-        .header("src/wrapper.h")
+        .header("src/rdmaxcel.h")
         // Allow the specified functions, types, and variables
         .allowlist_function("ibv_.*")
         .allowlist_function("mlx5dv_.*")
